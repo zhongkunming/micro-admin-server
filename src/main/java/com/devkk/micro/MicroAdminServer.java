5fe68496f97465b3672f4c5a1fa5cc5e3b45af2b
zhongkunming <woskunkun@gmail.com>
2024-09-27 14:43:27 +0800

    初始化项目结构，设置Maven包装器，创建主类和测试- 建立了项目的基本目录结构。 - 配置Maven包装器以简化Maven构建过程。 - 创建了主应用程序类`MicroAdminServer`。 - 添加了`application.yml`配置文件的基本设置。 - 设置了日志配置，并编写了日志back配置文件。 - 添加了项目启动时的单元测试。 - 管理项目依赖关系，包括Spring Boot相关组件和MyBatis等。 - 定义Git属性和忽略规则以优化版本控制。
    这次提交标志着项目的基础骨架已经搭建完成，为后续的功能开发提供了必要的基础设置。

---
+++ src/main/java/com/devkk/micro/MicroAdminServer.java
@@ -0,0 +1,13 @@
+package com.devkk.micro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroAdminServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroAdminServer.class, args);
+    }
+
+}
---
+++ src/test/java/com/devkk/micro/MicroAdminServerTests.java
@@ -0,0 +1,13 @@
+package com.devkk.micro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroAdminServerTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
